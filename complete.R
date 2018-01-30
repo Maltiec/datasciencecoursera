949a0de6f5a3f03e89fa4d49398de4cea37b586a
Maltiec <35667683+Maltiec@users.noreply.github.com>
2018-01-30 20:32:02 +0300

    Add files via upload

--- complete.R
+++ complete.R
@@ -1,24 +1,25 @@
 complete<-function(directory,id=1:10) {
-  complete_data<-data.frame(id=0,nobs=0)
+  print(id)
+  complete_data<-data.frame(id=integer(),nobs=integer(),stringsAsFactors=FALSE)
+  ##str(complete_data)
   file_list<-list.files(paste(getwd(),directory,sep="/"))
-  for (file_name in file_list){
-    complete_table<-data.frame(Date=0,sulfate=0,nitrate=0,ID=0)
-    current_table<-read.csv(paste(getwd(),directory,file_name,sep="/"))
-    for (current_id in id)  {
-      if (current_id %in% current_table[["ID"]]) {
-        not_na<-complete.cases(current_table)
-        complete_table<-rbind(complete_table,current_table[not_na,])
-        complete_table<-complete_table[-c(1),]
-        complete_data_nobs<-nrow(complete_table)
-        complete_data_id<-complete_table$ID[2]
-        complete_data<-rbind(complete_data,c(complete_data_id,complete_data_nobs))
-        ##print(complete_data_id)
-        ##print(mean_value)
-      }
-      
+  for (current_id in id)  {
+     for (file_name in file_list){
+        complete_table<-data.frame(Date=as.Date(character()),sulfate=double(),nitrate=double(),ID=integer(),stringsAsFactors=FALSE)
+        current_table<-read.csv(paste(getwd(),directory,file_name,sep="/"))
+        if (current_id %in% current_table[["ID"]]) {
+          not_na<-complete.cases(current_table)
+          complete_table<-rbind(complete_table,current_table[not_na,])
+          ##str(complete_table)
+          complete_data_nobs<-nrow(complete_table)
+          complete_data_id<-current_table$ID[1]
+          complete_data<-bind_rows(complete_data,c(id=complete_data_id,nobs=complete_data_nobs))
+          ## str(complete_data)
+          ##print(complete_data_id)
+          ##print(mean_value)
+        }
     }
-    
   }
-  complete_data<-complete_data[-c(1),]  
+ ## complete_data<-complete_data[-c(1),]  
   print(complete_data)
-  }
+}
